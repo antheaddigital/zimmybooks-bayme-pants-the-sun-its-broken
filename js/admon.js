436a00b9688b546aa027fbb7f521db8ca739076d
camachowebworld <camachowebworld@yahoo.com>
2016-08-02 21:11:46 -0700

    more ahhhh

--- js/admon.js
+++ js/admon.js
@@ -1,19 +1,23 @@
 function onDeviceReady() {
+  document.removeEventListener('deviceready', onDeviceReady, false);
 
   // Set AdMobAds options:
   admob.setOptions({
     publisherId: "ca-app-pub-4899785129776182/7652659352",
-    interstitialAdId: "ca-app-pub-4899785129776182/6653604159",
-    autoShowBanner: true
+    interstitialAdId: "ca-app-pub-4899785129776182/6653604159"
   });
 
   // Start showing banners (atomatic when autoShowBanner is set to true)
-  admob.createBannerView();
+  admob.createBannerView({
+    autoShowBanner: true
+  });
 
   // Request interstitial (will present automatically when autoShowInterstitial is set to true)
-  admob.requestInterstitialAd();
+  admob.requestInterstitialAd({
+    autoShowInterstitial: false
+  });
 }
 
-$(document).ready(function() {
-  onDeviceReady();
-});
+function domLoaded(){
+ document.addEventListener("deviceready", onDeviceReady, false);
+}
