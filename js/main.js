5361212d2190d2543c9d621f0661e51a5320bba4
camachowebworld <camachowebworld@yahoo.com>
2016-08-02 18:44:54 -0700

    testing admon again

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ $(document).ready(function(){
 
   $('.slider').on('afterChange', function(event, slick, currentSlide){
     if(currentSlide == 10){
-
+      // if(window.AdMob){
+      //   AdMob.showInterstitial();
+      // }
     }
   });
 
@@ -30,7 +32,11 @@ $(document).ready(function(){
   $('.slider').slick({
     infinite: true,
     dots: false,
-    cssEase: 'linear'
+    cssEase: 'linear',
+    onAfterChange: function(){
+      console.log('changed');
+      //$('.current').text($('.slides').slickCurrentSlide()+1);
+    }
   });
 
   $(window).on('resize', function(){
@@ -42,5 +48,55 @@ $(document).ready(function(){
   // Sign pop-up functionality
   $('.sign-link').magnificPopup({type:'image'});
 
+  /* ---------------------------------------------------------------------- */
+  // http://pointdeveloper.com/how-to-add-interstitial-ads-on-navigation-for-phonegap-app-using-admob/
+
+  // function adSetter(){
+  //   var admobid = {};
+  //   // select the right Ad Id according to platform
+  //   if( /(android)/i.test(navigator.userAgent) ) {
+  //     admobid = { // for Android
+  //       banner: 'ca-app-pub-4899785129776182~5176870955',
+  //       interstitial: 'ca-app-pub-4899785129776182/6653604159'
+  //     };
+  //   } else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
+  //     admobid = { // for iOS
+  //       //banner: 'ca-app-pub-6869992474017983/4806197152',
+  //       //interstitial: 'ca-app-pub-6869992474017983/7563979554'
+  //     };
+  //   } else {
+  //     admobid = { // for Windows Phone
+  //       //banner: 'ca-app-pub-6869992474017983/8878394753',
+  //       //interstitial: 'ca-app-pub-6869992474017983/1355127956'
+  //     };
+  //   }
+  //
+  //   if(window.AdMob){
+  //     AdMob.createBanner({
+  //       isTesting:true, //Remove this Before publishing your app
+  //       adId:admobid.banner,
+  //       position:AdMob.AD_POSITION.BOTTOM_CENTER,
+  //       autoShow:true
+  //     });
+  //   }
+  //
+  //   if(window.AdMob) AdMob.prepareInterstitial({
+  //     adId:admobid.interstitial,
+  //     autoShow:false
+  //   });
+  //
+  // }
+
+  function onDeviceReady(){
+    admob.requestInterstitialAd({
+      publisherId: "ca-app-pub-4899785129776182/5176870955",
+      interstitialAdId: "ca-app-pub-4899785129776182/6653604159"
+    });
+    //adSetter();
+  }
+
+  function domLoaded(){
+    document.addEventListener("deviceready", onDeviceReady, false);
+  }
 
 });
