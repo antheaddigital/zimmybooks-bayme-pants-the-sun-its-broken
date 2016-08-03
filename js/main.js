6a74381373e26627bc11e70f3f7ad48d4a245dcc
camachowebworld <camachowebworld@yahoo.com>
2016-08-02 19:21:06 -0700

    testing quick setup

--- js/main.js
+++ js/main.js
@@ -87,17 +87,35 @@ $(document).ready(function(){
   //
   // }
 
-  function onDeviceReady(){
-    admob.requestInterstitialAd({
-      publisherId: "ca-app-pub-4899785129776182/7652659352",
-      interstitialAdId: "ca-app-pub-4899785129776182/6653604159",
-      autoShowInterstitial: true
-    });
-    //adSetter();
-  }
+  function onDeviceReady() {
+      document.removeEventListener('deviceready', onDeviceReady, false);
 
-  function domLoaded(){
+      // Set AdMobAds options:
+      admob.setOptions({
+        publisherId:          "ca-app-pub-4899785129776182/7652659352",  // Required
+        interstitialAdId:     "ca-app-pub-4899785129776182/6653604159"  // Optional
+      });
+
+      // Start showing banners (atomatic when autoShowBanner is set to true)
+      admob.createBannerView();
+
+      // Request interstitial (will present automatically when autoShowInterstitial is set to true)
+      admob.requestInterstitialAd();
+    }
+
     document.addEventListener("deviceready", onDeviceReady, false);
-  }
+
+  // function onDeviceReady(){
+  //   admob.requestInterstitialAd({
+  //     publisherId: "ca-app-pub-4899785129776182/7652659352",
+  //     interstitialAdId: "ca-app-pub-4899785129776182/6653604159",
+  //     autoShowInterstitial: true
+  //   });
+  //   //adSetter();
+  // }
+  //
+  // function domLoaded(){
+  //   document.addEventListener("deviceready", onDeviceReady, false);
+  // }
 
 });
