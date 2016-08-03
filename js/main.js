6d0ad522828488a4b9ecdd480b4b2df42ffaa1ab
camachowebworld <camachowebworld@yahoo.com>
2016-08-02 18:00:53 -0700

    testing new id

--- js/main.js
+++ js/main.js
@@ -54,18 +54,18 @@ $(document).ready(function(){
     // select the right Ad Id according to platform
     if( /(android)/i.test(navigator.userAgent) ) {
       admobid = { // for Android
-        banner: 'ca-app-pub-6869992474017983/9375997553',
-        interstitial: 'ca-app-pub-6869992474017983/1657046752'
+        banner: 'ca-app-pub-4899785129776182~5176870955',
+        interstitial: 'ca-app-pub-4899785129776182/6653604159'
       };
     } else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
       admobid = { // for iOS
-        banner: 'ca-app-pub-6869992474017983/4806197152',
-        interstitial: 'ca-app-pub-6869992474017983/7563979554'
+        //banner: 'ca-app-pub-6869992474017983/4806197152',
+        //interstitial: 'ca-app-pub-6869992474017983/7563979554'
       };
     } else {
       admobid = { // for Windows Phone
-        banner: 'ca-app-pub-6869992474017983/8878394753',
-        interstitial: 'ca-app-pub-6869992474017983/1355127956'
+        //banner: 'ca-app-pub-6869992474017983/8878394753',
+        //interstitial: 'ca-app-pub-6869992474017983/1355127956'
       };
     }
 
