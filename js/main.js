98b640e4385fff6a17d673fb38e6ecb1e1aebcb4
camachowebworld <camachowebworld@yahoo.com>
2016-08-02 19:25:35 -0700

    set banner to auto true

--- js/main.js
+++ js/main.js
@@ -92,8 +92,9 @@ $(document).ready(function(){
 
       // Set AdMobAds options:
       admob.setOptions({
-        publisherId:          "ca-app-pub-4899785129776182/7652659352",  // Required
-        interstitialAdId:     "ca-app-pub-4899785129776182/6653604159"  // Optional
+        publisherId:          "ca-app-pub-4899785129776182/7652659352",
+        interstitialAdId:     "ca-app-pub-4899785129776182/6653604159",
+        autoShowBanner: true
       });
 
       // Start showing banners (atomatic when autoShowBanner is set to true)
