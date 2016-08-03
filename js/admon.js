265e0a888687b198ed4363fc3efc96ce639146e1
camachowebworld <camachowebworld@yahoo.com>
2016-08-02 20:36:34 -0700

    ummm trying anything

--- js/admon.js
+++ js/admon.js
@@ -1,5 +1,19 @@
-$(document).ready(function(){
+function onDeviceReady() {
 
+  // Set AdMobAds options:
+  admob.setOptions({
+    publisherId: "ca-app-pub-4899785129776182/7652659352",
+    interstitialAdId: "ca-app-pub-4899785129776182/6653604159",
+    autoShowBanner: true
+  });
 
+  // Start showing banners (atomatic when autoShowBanner is set to true)
+  admob.createBannerView();
 
+  // Request interstitial (will present automatically when autoShowInterstitial is set to true)
+  admob.requestInterstitialAd();
+}
+
+$(document).ready(function() {
+  onDeviceReady();
 });
--- js/main.js
+++ js/main.js
@@ -78,24 +78,7 @@ $(document).ready(function(){
   //
   // }
 
-  function onDeviceReady() {
-      document.removeEventListener('deviceready', onDeviceReady, false);
-
-      // Set AdMobAds options:
-      admob.setOptions({
-        publisherId:          "ca-app-pub-4899785129776182/7652659352",
-        interstitialAdId:     "ca-app-pub-4899785129776182/6653604159",
-        autoShowBanner: true
-      });
-
-      // Start showing banners (atomatic when autoShowBanner is set to true)
-      admob.createBannerView();
-
-      // Request interstitial (will present automatically when autoShowInterstitial is set to true)
-      admob.requestInterstitialAd();
-    }
-
-    document.addEventListener("deviceready", onDeviceReady, false);
+  
 
   // function onDeviceReady(){
   //   admob.requestInterstitialAd({
