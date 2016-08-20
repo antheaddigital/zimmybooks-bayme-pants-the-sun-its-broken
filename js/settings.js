b3a384330b7b427cefd46db5290d2ef4cdef6ebe
camachowebworld <camachowebworld@yahoo.com>
2016-08-20 12:11:20 -0700

    added admob id's for ios

--- js/settings.js
+++ js/settings.js
@@ -6,8 +6,8 @@ window.appSettings = {
       interstitial: 'ca-app-pub-4899785129776182/6653604159'
     },
     ios : {
-      banner: 'ca-app-pub-4899785129776182/7652659352',
-      interstitial: 'ca-app-pub-4899785129776182/6653604159'
+      banner: 'ca-app-pub-4899785129776182/3241185755',
+      interstitial: 'ca-app-pub-4899785129776182/4717918950'
     }
   },
   piwik: {
